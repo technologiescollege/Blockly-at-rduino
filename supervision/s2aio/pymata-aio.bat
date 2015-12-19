6baf2438e8aa41ee502f3fbe87900cdeb45aaba2
SebCanet <sebastien.canet@ac-nantes.fr>
2015-12-19 14:10:24 +0100

    cleaning

--- supervision/s2aio/pymata-aio.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-break ON
-rem fichiers BAT et fork créés par Sébastien CANET
-cls
-SET currentpath=%~dp1
-cd %currentpath%
-SET /p portCOM=Numero du port COM ?
-python .\Lib\site-packages\pymata_aio\pymata_iot.py -l 5 -c no_client -comport COM%portCOM%
--- supervision/s2aio/s2aio-py.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-break ON
-rem fichiers BAT et fork créés par Sébastien CANET
-cls
-SET currentpath=%~dp1
-cd %currentpath%
-SET /p portCOM=Numero du port COM ?
-python .\Lib\site-packages\s2aio\__main__.py -l 5 -c no_client -p COM%portCOM%
