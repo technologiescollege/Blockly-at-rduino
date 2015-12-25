8facfc4701efff4636b0f247aad647e47be6f0d3
SebCanet <sebastien.canet@ac-nantes.fr>
2015-12-25 03:47:02 +0100

    Revert "test"
    This reverts commit 8f50697024f8aaba10fe6bc3e680a847918bb912.

---
+++ generators/arduino/constant.js
@@ -0,0 +1,56 @@
+/**
+ * Visual Blocks Language
+ *
+ * Copyright 2012 Google Inc.
+ * http://blockly.googlecode.com/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileoverview Variable blocks for Arduino.
+ * @author gasolin@gmail.com (Fred Lin)
+ */
+'use strict';
+
+goog.provide('Blockly.Arduino.constant');
+
+goog.require('Blockly.Arduino');
+
+Blockly.Arduino.constant_get = function() {
+  // Constant getter.
+  var code = Blockly.Arduino.variableDB_.getName(this.getFieldValue('CONST'),
+      Blockly.Variables.NAME_TYPE);
+  return [code, Blockly.Arduino.ORDER_ATOMIC];
+};
+
+Blockly.Arduino.constant_declare = function() {
+  // Constant setter.
+  var dropdown_type = this.getFieldValue('TYPE');
+  //TODO: settype to Constant
+  var argument0 = Blockly.Arduino.valueToCode(this, 'VALUE',
+      Blockly.Arduino.ORDER_ASSIGNMENT) || '0';
+  var varName = Blockly.Arduino.variableDB_.getName(this.getFieldValue('CONST'),
+      Blockly.Variables.NAME_TYPE);
+  Blockly.Arduino.setups_['setup_var' + varName] = varName + ' = ' + argument0 + ';\n';
+  return '';
+};
+
+Blockly.Arduino.constant_set = function() {
+  // Constant setter.
+  var argument0 = Blockly.Arduino.valueToCode(this, 'VALUE',
+      Blockly.Arduino.ORDER_ASSIGNMENT) || '0';
+  var varName = Blockly.Arduino.variableDB_.getName(this.getFieldValue('CONST'),
+      Blockly.Variables.NAME_TYPE);
+  return varName + ' = ' + argument0 + ';\n';
+};
