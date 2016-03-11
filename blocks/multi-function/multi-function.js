a9eb0cd3c04f1d3a1edfb5450dfb2b899f691c33
SebCanet <sebastien.canet@ac-nantes.fr>
2016-03-11 02:53:25 +0100

    multifunction shield

--- blocks/multi-function/multi-function.js
+++ blocks/multi-function/multi-function.js
@@ -33,15 +33,16 @@ Blockly.Blocks.multifunction_buildin_led = {
     this.setHelpUrl(Blockly.Msg.multifunction_HELPURL);
 	this.appendDummyInput()
         .appendField(new Blockly.FieldImage(Blockly.pathToBlockly + 'blocks/multi-function/DEL.jpg', Blockly.Arduino.imageSize, Blockly.Arduino.imageSize))
-      	.appendField(Blockly.Msg.ARDUINO_INOUT_BUILDIN_LED_INPUT)
-		.appendField(new Blockly.FieldDropdown([["D1", "D1"], ["D2", "D2"], ["D3", "D3"], ["D4", "D4"]]), 'DEL');
+      	.appendField(Blockly.Msg.multifunction_buildin_led_INPUT)
+		.appendField(new Blockly.FieldDropdown([["D1", "13"], ["D2", "12"], ["D3", "11"], ["D4", "10"]]), 'DEL');
 	this.appendDummyInput()
-      	.appendField(Blockly.Msg.ARDUINO_INOUT_STAT)		
+      	.appendField(Blockly.Msg.multifunction_buildin_led_STAT)		
 		.appendField(new Blockly.FieldDropdown(Blockly.Msg.FIELDDROPDOWN), 'STAT');
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);	 
     this.setOutput(null);
-    this.setTooltip(Blockly.Msg.ARDUINO_INOUT_BUILDIN_LED_TOOLTIP);
+    this.setInputsInline(true);
+    this.setTooltip(Blockly.Msg.multifunction_buildin_led_TOOLTIP);
    }
 };
 
@@ -51,11 +52,11 @@ Blockly.Blocks.multifunction_digital_read = {
     this.setHelpUrl(Blockly.Msg.multifunction_HELPURL);
 	this.appendDummyInput()
         .appendField(new Blockly.FieldImage(Blockly.pathToBlockly + 'blocks/multi-function/BP.jpg', Blockly.Arduino.imageSize, Blockly.Arduino.imageSize))
-      	.appendField(Blockly.Msg.ARDUINO_INOUT_BUILDIN_LED_INPUT)
+      	.appendField(Blockly.Msg.multifunction_digital_read_INPUT)
         .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField(new Blockly.FieldDropdown([["S1", "S1"], ["S2", "S2"], ["S3", "S3"]]), 'BP');
+        .appendField(new Blockly.FieldDropdown([["S1", "A1"], ["S2", "A2"], ["S3", "A3"]]), 'BP');
     this.setOutput(true, 'Boolean');
-    this.setTooltip(Blockly.Msg.ARDUINO_INOUT_DIGITAL_READ_TOOLTIP);
+    this.setTooltip(Blockly.Msg.multifunction_digital_read_TOOLTIP);
   }
 };
 
@@ -65,10 +66,10 @@ Blockly.Blocks.multifunction_pot_read = {
 	this.setHelpUrl(Blockly.Msg.multifunction_HELPURL);
 	this.appendDummyInput()
         .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField(Blockly.Msg.ARDUINO_INOUT_ANALOG_READ_INPUT)
+        .appendField(Blockly.Msg.multifunction_pot_read_INPUT)
         .appendField(new Blockly.FieldImage(Blockly.pathToBlockly + 'blocks/multi-function/pot.jpg', Blockly.Arduino.imageSize, Blockly.Arduino.imageSize));
     this.setOutput(true, 'Number');
-    this.setTooltip(Blockly.Msg.ARDUINO_INOUT_ANALOG_READ_TOOLTIP);
+    this.setTooltip(Blockly.Msg.multifunction_pot_read_TOOLTIP);
   }
 };
 
@@ -78,25 +79,10 @@ Blockly.Blocks.multifunction_analog_read = {
 	this.setHelpUrl(Blockly.Msg.multifunction_HELPURL);
 	this.appendDummyInput()
         .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField(Blockly.Msg.ARDUINO_INOUT_ANALOG_READ_INPUT)
+        .appendField(Blockly.Msg.multifunction_analog_read_INPUT)
         .appendField(new Blockly.FieldImage(Blockly.pathToBlockly + 'blocks/multi-function/A5.jpg', Blockly.Arduino.imageSize, Blockly.Arduino.imageSize));
     this.setOutput(true, 'Number');
-    this.setTooltip(Blockly.Msg.ARDUINO_INOUT_ANALOG_READ_TOOLTIP);
-  }
-};
-
-Blockly.Blocks.multifunction_PWM_write = {
-  init: function() {
-    this.setColour("#0152AB");
-    this.setHelpUrl(Blockly.Msg.multifunction_HELPURL);
-	this.appendDummyInput()
-        .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField(new Blockly.FieldImage(Blockly.pathToBlockly + 'blocks/multi-function/servo.jpg', Blockly.Arduino.imageSize, Blockly.Arduino.imageSize))
-        .appendField(new Blockly.FieldDropdown([["~5", "5"], ["~6", "6"], ["~9", "9"]]), 'PIN');
-    this.setInputsInline(false);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setTooltip(Blockly.Msg.ARDUINO_INOUT_PWM_WRITE_TOOLTIP);
+    this.setTooltip(Blockly.Msg.multifunction_analog_read_TOOLTIP);
   }
 };
 
@@ -106,20 +92,20 @@ Blockly.Blocks.multifunction_tone = {
     this.setHelpUrl(Blockly.Msg.multifunction_HELPURL);
 	this.appendDummyInput()
         .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField(Blockly.Msg.ARDUINO_TONE_INPUT1)
+        .appendField(Blockly.Msg.multifunction_tone_INPUT1)
         .appendField(new Blockly.FieldImage(Blockly.pathToBlockly + 'blocks/multi-function/buzzer.jpg', Blockly.Arduino.imageSize, Blockly.Arduino.imageSize));
-    this.appendValueInput("NUM", 'Number')
+    this.appendValueInput("NUM")
         .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField(Blockly.Msg.ARDUINO_TONE_INPUT2)
+        .appendField(Blockly.Msg.multifunction_tone_INPUT2)
         .setCheck('Number');
-    this.appendValueInput("TPS", 'Number')
+    this.appendValueInput("TPS")
         .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField(Blockly.Msg.ARDUINO_TONE_INPUT3)
+        .appendField(Blockly.Msg.multifunction_tone_INPUT3)
         .setCheck('Number');
     this.setInputsInline(false);
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
-    this.setTooltip(Blockly.Msg.ARDUINO_TONE_TOOLTIP);
+    this.setTooltip(Blockly.Msg.multifunction_tone_TOOLTIP);
   }
 };
 
@@ -127,12 +113,12 @@ Blockly.Blocks.multifunction_notone = {
   init: function() {
     this.setColour("#0152AB");
 	this.setHelpUrl(Blockly.Msg.multifunction_HELPURL);
-	this.appendValueInput("PIN", 'Number')
-        .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField(Blockly.Msg.ARDUINO_NOTONE_INPUT);
+	this.appendDummyInput()
+        .setAlign(Blockly.ALIGN_LEFT)
+        .appendField(Blockly.Msg.multifunction_notone_INPUT);
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
-    this.setTooltip(Blockly.Msg.ARDUINO_NOTONE_TOOLTIP);
+    this.setTooltip(Blockly.Msg.multifunction_notone_TOOLTIP);
   }
 };
 
@@ -141,21 +127,66 @@ Blockly.Blocks.multifunction_segment = {
     this.setColour("#0152AB");
 	this.setHelpUrl(Blockly.Msg.multifunction_HELPURL);
 	this.appendDummyInput()
+		.setAlign(Blockly.ALIGN_LEFT)
+        .appendField(Blockly.Msg.multifunction_segment_TEXT)
         .appendField(new Blockly.FieldImage(Blockly.pathToBlockly + 'blocks/multi-function/segments.jpg', Blockly.Arduino.imageSize, Blockly.Arduino.imageSize))
-	this.appendValueInput("NUM1", 'Number')
+	this.appendValueInput("NUM1")
         .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField(Blockly.Msg.ARDUINO_INOUT_ANALOG_READ_INPUT);
-	this.appendValueInput("NUM2", 'Number')
+        .appendField(Blockly.Msg.multifunction_segment_INPUT1)
+        .setCheck('Number');
+	this.appendValueInput("NUM2")
         .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField(Blockly.Msg.ARDUINO_INOUT_ANALOG_READ_INPUT);
-	this.appendValueInput("NUM3", 'Number')
+        .appendField(Blockly.Msg.multifunction_segment_INPUT2)
+        .setCheck('Number');
+	this.appendValueInput("NUM3")
         .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField(Blockly.Msg.ARDUINO_INOUT_ANALOG_READ_INPUT);
-	this.appendValueInput("NUM4", 'Number')
+        .appendField(Blockly.Msg.multifunction_segment_INPUT3)
+        .setCheck('Number');
+	this.appendValueInput("NUM4")
         .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField(Blockly.Msg.ARDUINO_INOUT_ANALOG_READ_INPUT);
+        .appendField(Blockly.Msg.multifunction_segment_INPUT4)
+        .setCheck('Number');
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
-    this.setTooltip(Blockly.Msg.ARDUINO_INOUT_ANALOG_READ_TOOLTIP);
+    this.setTooltip(Blockly.Msg.multifunction_segment_TOOLTIP);
+  }
+};
+
+Blockly.Blocks.multifunction_segment_number = {
+  init: function() {
+    this.setColour("#0152AB");
+	this.setHelpUrl(Blockly.Msg.multifunction_HELPURL);
+	this.appendDummyInput()
+		.setAlign(Blockly.ALIGN_LEFT)
+        .appendField(new Blockly.FieldImage(Blockly.pathToBlockly + 'blocks/multi-function/segments.jpg', Blockly.Arduino.imageSize, Blockly.Arduino.imageSize))
+	this.appendValueInput("NUM")
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField(Blockly.Msg.multifunction_segment_TEXT)
+        .setCheck('Number');
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setTooltip(Blockly.Msg.multifunction_segment_TOOLTIP);
+  }
+};
+
+Blockly.Blocks.multifunction_PWM_write = {
+  init: function() {
+    this.setColour("#0152AB");
+    this.setHelpUrl(Blockly.Msg.multifunction_HELPURL);
+	this.appendDummyInput()
+        .setAlign(Blockly.ALIGN_LEFT)
+        .appendField(Blockly.Msg.multifunction_PWM_write_OUTPUT);
+	this.appendDummyInput()
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField(new Blockly.FieldImage(Blockly.pathToBlockly + 'blocks/multi-function/servo.jpg', Blockly.Arduino.imageSize, Blockly.Arduino.imageSize))
+        .appendField(new Blockly.FieldDropdown([["~5", "5"], ["~6", "6"], ["~9", "9"]]), 'PIN');
+	this.appendValueInput("NUM")
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField(Blockly.Msg.multifunction_PWM_write_ANGLE)
+        .setCheck('Number');
+    this.setInputsInline(false);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setTooltip(Blockly.Msg.multifunction_PWM_write_TOOLTIP);
   }
 };
---
+++ generators/arduino/multi-function.js
@@ -0,0 +1,146 @@
+/**
+ * Visual Blocks Language
+ *
+ * Copyright 2012 Fred Lin.
+ * https://github.com/gasolin/BlocklyDuino
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileoverview Helper functions for generating Arduino blocks.
+ * @author gasolin@gmail.com (Fred Lin)
+ */
+'use strict';
+
+goog.provide('Blockly.Arduino.multi-function');
+
+goog.require('Blockly.Arduino');
+
+Blockly.Arduino.multifunction_buildin_led = function() {
+  var dropdown_stat = this.getFieldValue('STAT');
+  var dropdown_DEL = this.getFieldValue('DEL');
+  Blockly.Arduino.setups_['setup_buildin_LED_' + dropdown_DEL] = 'pinMode(' + dropdown_DEL + ', OUTPUT);';
+  var code = 'digitalWrite(' + dropdown_DEL + ', ' + '!' + dropdown_stat + ');\n';
+  return code;
+};
+
+Blockly.Arduino.multifunction_digital_read = function() {
+  var dropdown_BP = this.getFieldValue('BP');
+  var code = 'digitalRead(' + dropdown_BP + ')';
+  return [code, Blockly.Arduino.ORDER_ATOMIC];
+};
+
+Blockly.Arduino.multifunction_pot_read = function() {
+  var code = 'analogRead(A0)';
+  return [code, Blockly.Arduino.ORDER_ATOMIC];
+};
+
+Blockly.Arduino.multifunction_analog_read = function() {
+  var code = 'analogRead(A5)';
+  return [code, Blockly.Arduino.ORDER_ATOMIC];
+};
+
+Blockly.Arduino.multifunction_tone = function() {
+  var value_num = Blockly.Arduino.valueToCode(this, 'NUM', Blockly.Arduino.ORDER_ATOMIC);
+  var value_tps = Blockly.Arduino.valueToCode(this, 'TPS', Blockly.Arduino.ORDER_ATOMIC);
+  Blockly.Arduino.setups_['setup_multifunction_buzzer'] = 'pinMode(3, OUTPUT);';
+  var code = 'tone(3,'+value_num+','+value_tps+');\n';
+  return code;
+};
+
+Blockly.Arduino.multifunction_notone = function() {
+  Blockly.Arduino.setups_['setup_multifunction_buzzer'] = 'pinMode(3, OUTPUT);';
+  var code = 'noTone(3);\n';
+  return code;
+};
+
+Blockly.Arduino.multifunction_segment = function() {
+  var value_num1 = Blockly.Arduino.valueToCode(this, 'NUM1', Blockly.Arduino.ORDER_ATOMIC);
+  var value_num2 = Blockly.Arduino.valueToCode(this, 'NUM2', Blockly.Arduino.ORDER_ATOMIC);
+  var value_num3 = Blockly.Arduino.valueToCode(this, 'NUM3', Blockly.Arduino.ORDER_ATOMIC);
+  var value_num4 = Blockly.Arduino.valueToCode(this, 'NUM4', Blockly.Arduino.ORDER_ATOMIC);
+  
+  Blockly.Arduino.definitions_['setup_multifunction_segments'] = '#define LATCH_DIO 4\n' +
+  '#define CLK_DIO 7\n' +
+  '#define DATA_DIO 8\n\n' + 
+  '/* Segment byte maps for numbers 0 to 9 */\n' +
+  'const byte SEGMENT_MAP[] = {0xC0,0xF9,0xA4,0xB0,0x99,0x92,0x82,0xF8,0X80,0X90};\n\n' +
+  '/* Byte maps to select digit 1 to 4 */\n' +
+  'const byte SEGMENT_SELECT[] = {0xF1,0xF2,0xF4,0xF8};;\n\n' +
+  '/* Write a decimal number between 0 and 9999 to the display */\n' +
+  'void WriteNumber(int Number)\n' +
+  '  {\n' +
+  '  WriteNumberToSegment(0 , ' + value_num1 + ');\n' +
+  '  WriteNumberToSegment(1 , ' + value_num2 + ' % 10);\n' +
+  '  WriteNumberToSegment(2 , ' + value_num3 + ' % 10);\n' +
+  '  WriteNumberToSegment(3 , ' + value_num4 + ' % 10);\n' +
+  '  }\n\n' +
+  '/* Wite a ecimal number between 0 and 9 to one of the 4 digits of the display */\n' +
+  'void WriteNumberToSegment(byte Segment, byte Value)\n' +
+  '  {\n' +
+  '  digitalWrite(LATCH_DIO,LOW);\n' +
+  '  shiftOut(DATA_DIO, CLK_DIO, MSBFIRST, SEGMENT_MAP[Value]);\n' +
+  '  shiftOut(DATA_DIO, CLK_DIO, MSBFIRST, SEGMENT_SELECT[Segment] );\n' +
+  '  digitalWrite(LATCH_DIO,HIGH);\n' +
+  '  }\n\n';
+  
+  Blockly.Arduino.setups_['setup_multifunction_segments'] = '/* Set DIO pins to outputs */\n' +
+  'pinMode(LATCH_DIO,OUTPUT);\n' +
+  'pinMode(CLK_DIO,OUTPUT);\n' +
+  'pinMode(DATA_DIO,OUTPUT);\n';
+  var code = 'WriteNumber(' + value_num1 + value_num2 + value_num3 + value_num4 + ');\n';
+  return code;
+};
+
+Blockly.Arduino.multifunction_segment_number = function() {
+  var value_num = Blockly.Arduino.valueToCode(this, 'NUM', Blockly.Arduino.ORDER_ATOMIC);
+  
+  Blockly.Arduino.definitions_['setup_multifunction_segments'] = '#define LATCH_DIO 4\n' +
+  '#define CLK_DIO 7\n' +
+  '#define DATA_DIO 8\n\n' + 
+  '/* Segment byte maps for numbers 0 to 9 */\n' +
+  'const byte SEGMENT_MAP[] = {0xC0,0xF9,0xA4,0xB0,0x99,0x92,0x82,0xF8,0X80,0X90};\n\n' +
+  '/* Byte maps to select digit 1 to 4 */\n' +
+  'const byte SEGMENT_SELECT[] = {0xF1,0xF2,0xF4,0xF8};;\n\n' +
+  '/* Write a decimal number between 0 and 9999 to the display */\n' +
+  'void WriteNumber(int Number)\n' +
+  '  {\n' +
+  '  WriteNumberToSegment(0 , ' + value_num + '/1000);\n' +
+  '  WriteNumberToSegment(1 , ' + value_num + '/100 % 10);\n' +
+  '  WriteNumberToSegment(2 , ' + value_num + '/10 % 10);\n' +
+  '  WriteNumberToSegment(3 , ' + value_num + ' % 10);\n' +
+  '  }\n\n' +
+  '/* Wite a ecimal number between 0 and 9 to one of the 4 digits of the display */\n' +
+  'void WriteNumberToSegment(byte Segment, byte Value)\n' +
+  '  {\n' +
+  '  digitalWrite(LATCH_DIO,LOW);\n' +
+  '  shiftOut(DATA_DIO, CLK_DIO, MSBFIRST, SEGMENT_MAP[Value]);\n' +
+  '  shiftOut(DATA_DIO, CLK_DIO, MSBFIRST, SEGMENT_SELECT[Segment] );\n' +
+  '  digitalWrite(LATCH_DIO,HIGH);\n' +
+  '  }\n\n';
+  
+  Blockly.Arduino.setups_['setup_multifunction_segments'] = '/* Set DIO pins to outputs */\n' +
+  'pinMode(LATCH_DIO,OUTPUT);\n' +
+  'pinMode(CLK_DIO,OUTPUT);\n' +
+  'pinMode(DATA_DIO,OUTPUT);\n';
+  var code = 'WriteNumber(' + value_num + ');\n';
+  return code;
+};
+Blockly.Arduino.multifunction_PWM_write = function() {
+  var dropdown_pin = this.getFieldValue('PIN');
+  var value_num = Blockly.Arduino.valueToCode(this, 'NUM', Blockly.Arduino.ORDER_ATOMIC);
+  Blockly.Arduino.setups_['setup_output'+dropdown_pin] = 'pinMode('+dropdown_pin+', OUTPUT);';
+  var code = 'analogWrite(' + dropdown_pin + ', ' + value_num + ');\n';
+  return code;
+};
