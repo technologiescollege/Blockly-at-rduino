e73640039be030b7d1b538bf37c62b5e0d383c01
SebCanet <sebastien.canet@ac-nantes.fr>
2016-03-15 18:49:51 +0100

    few bugs corrected

--- generators/arduino/softserial.js
+++ generators/arduino/softserial.js
@@ -6,15 +6,15 @@ Blockly.Arduino['soft_init'] = function(block) {
   var dropdown_pin1 = this.getFieldValue('PIN1');
   var dropdown_pin2 = this.getFieldValue('PIN2');
   var dropdown_speed = this.getFieldValue('SPEED');
-  Blockly.Arduino.definitions_['define_ss'] = '#include <SoftwareSerial.h>\nSoftwareSerial mySerial' +dropdown_pin1+ '('+dropdown_pin1+','+dropdown_pin2+');\n';
-  Blockly.Arduino.setups_['sserial_begin' + dropdown_pin1] = 'mySerial' + dropdown_pin1 + '.begin(' + dropdown_speed + ');' ;
+  Blockly.Arduino.definitions_['define_ss'] = '#include <SoftwareSerial.h>\nSoftwareSerial mySerial('+dropdown_pin1+','+dropdown_pin2+');\n';
+  Blockly.Arduino.setups_['setup_sserial'] = 'mySerial.begin(' + dropdown_speed + ');' ;
   return '';
 };
 
 Blockly.Arduino['soft_read'] = function(block) {
   // TODO: Assemble Python into code variable.
-  Blockly.Arduino.setups_['setup_sserial'] = 'mySerial.begin(9600);\n';
-  var code = '(mySerial.read())';
+  //Blockly.Arduino.setups_['setup_sserial'] = 'mySerial.begin(9600);\n';
+  var code = 'mySerial.read()';
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.Arduino.ORDER_ATOMIC];
 };
@@ -23,7 +23,7 @@ Blockly.Arduino['soft_print'] = function(block) {
   var content = Blockly.Arduino.valueToCode(this, 'CONTENT', Blockly.Arduino.ORDER_ATOMIC) || '0';
   //content = content.replace('(','').replace(')','');
 
-  Blockly.Arduino.setups_['setup_sserial'] = 'mySerial.begin(9600);\n';
+  //Blockly.Arduino.setups_['setup_sserial'] = 'mySerial.begin(9600);\n';
 
   var code = 'mySerial.println('+content+');\n';
   return code;
@@ -33,7 +33,7 @@ Blockly.Arduino['soft_write'] = function(block) {
   var content = Blockly.Arduino.valueToCode(this, 'CONTENT', Blockly.Arduino.ORDER_ATOMIC) || '0';
   //content = content.replace('(','').replace(')','');
 
-  Blockly.Arduino.setups_['setup_sserial'] = 'mySerial.begin(9600);\n';
+  //Blockly.Arduino.setups_['setup_sserial'] = 'mySerial.begin(9600);\n';
 
   var code = 'mySerial.write('+content+');\n';
   return code;
@@ -41,7 +41,7 @@ Blockly.Arduino['soft_write'] = function(block) {
 
 Blockly.Arduino['soft_available'] = function(block) {
   // TODO: Assemble Python into code variable.
-   Blockly.Arduino.setups_['setup_sserial'] = 'mySerial.begin(9600);\n';
+  // Blockly.Arduino.setups_['setup_sserial'] = 'mySerial.begin(9600);\n';
   var code = 'mySerial.available()';
 
   // TODO: Change ORDER_NONE to the correct strength.
