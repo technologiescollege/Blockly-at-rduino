aae5c7b7f24fc82738e4d40472868ee7a8e45d1e
SebCanet <sebastien.canet@ac-nantes.fr>
2016-04-04 20:05:12 +0200

    correct Grove T° sensor

--- generators/arduino/grove.js
+++ generators/arduino/grove.js
@@ -83,7 +83,7 @@ Blockly.Arduino.grove_temporature_sensor = function() {
   resistance=(float)(1023-a)*10000/a;
   temperature=1/(log(resistance/10000)/B+1/298.15)-273.15;
   */
-  var code = 'round('+'(1/(log((float)(1023-analogRead('+dropdown_pin+'))*10000/analogRead('+dropdown_pin+'))/10000)/3975+1/298.15)-273.15'+')';
+  var code = 'round('+'(1/(log((float)(1023-analogRead('+dropdown_pin+'))*10000/analogRead('+dropdown_pin+')/10000)/3975+1/298.15)-273.15'+')';
   Blockly.Arduino.setups_['setup_temporature_sensor_'+dropdown_pin] = 'pinMode('+dropdown_pin+', INPUT);';
   return [code, Blockly.Arduino.ORDER_ATOMIC];
 };
