f6d63d4064bb3d787297f2aea7dfe3cb84eb540e
SebCanet <sebastien.canet@ac-nantes.fr>
2016-08-21 18:43:06 +0200

    added FlyCamOne Eco v2 finished cleaning

--- blocks/blocks_typing.js
+++ blocks/blocks_typing.js
@@ -228,3 +228,9 @@ Blockly.Blocks.bq_led.getBlockType = function() {
 Blockly.Blocks.flycam_switch.getBlockType = function() {
 	return Blockly.Types.NUMBER;
 };
+Blockly.Blocks.flycam_record.getBlockType = function() {
+	return Blockly.Types.NUMBER;
+};
+Blockly.Blocks.flycam_stop.getBlockType = function() {
+	return Blockly.Types.NUMBER;
+};
