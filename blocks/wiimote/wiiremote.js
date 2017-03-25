959104e323cae6e293da6f3047a355a1d2fce3c8
SebCanet <scanet@libreduc.cc>
2017-03-25 15:12:27 +0100

    add grove blocks

---
+++ blocks/wiimote/wiiremote.js
@@ -0,0 +1,72 @@
+﻿// define blocks
+'use strict';
+
+goog.provide('Blockly.Blocks.wiiremote');
+
+goog.require('Blockly.Blocks');
+goog.require('Blockly.Types');
+goog.require('Blockly.FieldInstance');
+
+Blockly.Blocks.wiiremote_init = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField(Blockly.Msg.lp2i_ledRGB_WS2812B_init)
+        .setAlign(Blockly.ALIGN_RIGHT)
+		.appendField(
+				new Blockly.FieldInstance('WS2812_fieldInstance',
+										  Blockly.Msg.lp2i_ledRGB_WS2812B_DEFAULT_NAME,
+										  true, true, false),
+				'NEOPIXEL_NAME')
+		.appendField(new Blockly.FieldImage(Blockly.pathToBlockly + 'blocks/led-rgb-ws2812b/led-rgb-ws2812b.jpg', Blockly.Arduino.imageSize, Blockly.Arduino.imageSize));
+    this.appendValueInput("Pin_LedRGB_init")
+		.setCheck('Number')
+        .setAlign(Blockly.ALIGN_RIGHT)
+		.appendField(Blockly.Msg.lp2i_ledRGB_WS2812B_init_Pin);
+    this.appendValueInput("Number_of_Pixels")
+		.setCheck('Number')
+        .setAlign(Blockly.ALIGN_RIGHT)
+		.appendField(Blockly.Msg.lp2i_ledRGB_WS2812B_init_Number_of_Pixels);
+    this.setInputsInline(false);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(Blockly.Blocks.ledRGB_WS2812B.HUE);
+    this.setTooltip('');
+    this.setHelpUrl('http://blogpeda.ac-poitiers.fr/techno-jean-mace/2016/02/07/utilisation-de-modules-led-rgb-ws2812b-avec-blockly-arduino/');
+  }
+};
+
+Blockly.Blocks.wiiremote_calibrate = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField(Blockly.Msg.lp2i_ledRGB_WS2812B_init)
+        .setAlign(Blockly.ALIGN_RIGHT)
+		.appendField(
+				new Blockly.FieldInstance('WS2812_fieldInstance',
+										  Blockly.Msg.lp2i_ledRGB_WS2812B_DEFAULT_NAME,
+										  true, true, false),
+				'NEOPIXEL_NAME')
+		.appendField(new Blockly.FieldImage(Blockly.pathToBlockly + 'blocks/led-rgb-ws2812b/led-rgb-ws2812b.jpg', Blockly.Arduino.imageSize, Blockly.Arduino.imageSize));
+    this.appendValueInput("Pin_LedRGB_init")
+		.setCheck('Number')
+        .setAlign(Blockly.ALIGN_RIGHT)
+		.appendField(Blockly.Msg.lp2i_ledRGB_WS2812B_init_Pin);
+    this.appendValueInput("Number_of_Pixels")
+		.setCheck('Number')
+        .setAlign(Blockly.ALIGN_RIGHT)
+		.appendField(Blockly.Msg.lp2i_ledRGB_WS2812B_init_Number_of_Pixels);
+    this.setInputsInline(false);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(Blockly.Blocks.ledRGB_WS2812B.HUE);
+    this.setTooltip('');
+    this.setHelpUrl('http://blogpeda.ac-poitiers.fr/techno-jean-mace/2016/02/07/utilisation-de-modules-led-rgb-ws2812b-avec-blockly-arduino/');
+  }
+};
+
+Blockly.Blockswiiremote_accel = {
+
+};
+
+Blockly.Blockswiiremote_button = {
+
+};
---
+++ generators/arduino/wiiremote.js
@@ -0,0 +1,40 @@
+'use strict';
+
+goog.provide('Blockly.Arduino.ledRGB_WS2812B');
+
+goog.require('Blockly.Arduino');
+
+Blockly.Arduino.lp2i_ledRGB_WS2812B_init = function() {
+  var pin_ledrgb = Blockly.Arduino.valueToCode(this, 'Pin_LedRGB_init', Blockly.Arduino.ORDER_ATOMIC);
+  var numpixels = Blockly.Arduino.valueToCode(this, 'Number_of_Pixels', Blockly.Arduino.ORDER_ATOMIC);
+  var lumin = Blockly.Arduino.valueToCode(this, 'Brightness', Blockly.Arduino.ORDER_ATOMIC);
+  var dropdown_name = this.getFieldValue('NEOPIXEL_NAME');
+  Blockly.Arduino.includes_['define_ledRGB_WS2812B'] = '#include <Adafruit_NeoPixel.h>';
+  Blockly.Arduino.definitions_['define_ledRGB_WS2812B' + dropdown_name] = 'Adafruit_NeoPixel pixels_'+dropdown_name+ ' = Adafruit_NeoPixel('+numpixels+', '+pin_ledrgb+', NEO_GRB + NEO_KHZ800);';
+  //dans le setup
+  Blockly.Arduino.setups_['setup_pin_ledrgb'+dropdown_name] = 'pinMode('+pin_ledrgb+', OUTPUT);';
+  Blockly.Arduino.setups_['setup_ledRGB_WS2812B'+dropdown_name] = 'pixels_'+dropdown_name+'.begin();';
+  return '';
+};
+
+Blockly.Arduino.lp2i_ledRGB_WS2812B_setPixelColor = function() {
+  var pin = Blockly.Arduino.valueToCode(this, 'Pin_LedRGB', Blockly.Arduino.ORDER_ATOMIC);
+  var pixel_number = Blockly.Arduino.valueToCode(this, 'Pixel_number', Blockly.Arduino.ORDER_ATOMIC) || '\'\'';
+  var dropdown_name = this.getFieldValue('NEOPIXEL_NAME');
+  var red = Blockly.Arduino.valueToCode(this, 'Red', Blockly.Arduino.ORDER_ATOMIC);
+  var green = Blockly.Arduino.valueToCode(this, 'Green', Blockly.Arduino.ORDER_ATOMIC);
+  var blue = Blockly.Arduino.valueToCode(this, 'Blue', Blockly.Arduino.ORDER_ATOMIC);
+
+  var code = 'pixels_'+dropdown_name+'.setPixelColor('+pixel_number+', pixels'+pin+'.Color('+red+','+green+','+blue+'));\n'
+			+ 'pixels_'+dropdown_name+'.show();\n';
+  return code;
+};
+
+Blockly.Arduino.lp2i_ledRGB_WS2812B_setBrightness = function() {
+  //var pin_ledrgb = Blockly.Arduino.valueToCode(this, 'Pin_LedRGB', Blockly.Arduino.ORDER_ATOMIC);
+  var dropdown_name = this.getFieldValue('NEOPIXEL_NAME');
+  var lumin = Blockly.Arduino.valueToCode(this, 'Brightness', Blockly.Arduino.ORDER_ATOMIC);
+  //dans le setup
+  var code = 'pixels_'+dropdown_name+'.setBrightness('+lumin+');\n';
+  return code;
+};
