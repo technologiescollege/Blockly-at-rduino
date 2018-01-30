0e65f2bba020c35e4633d1bd89d412e309121600
bzhtitude <gwendal.jugand@ac-rouen.fr>
2018-01-30 22:33:26 +0100

    Ajout + petites modifs

--- blocks/ethernet/blocks_typing.js
+++ blocks/ethernet/blocks_typing.js
@@ -4,7 +4,7 @@
 
 //****************************blynk_cc.js********************
 
-Blockly.Blocks.ethernet_begin_dhcp.getBlockType = function() {
+Blockly.Blocks.ethernet_begin_dhcp_client.getBlockType = function() {
    return Blockly.Types.NUMBER;
 };
 Blockly.Blocks.ethernet_mac_address.getBlockType = function() {
@@ -22,10 +22,10 @@ Blockly.Blocks.ethernet_localip.getBlockType = function() {
    return Blockly.Types.NUMBER;
 };
 Blockly.Blocks.ethernet_available.getBlockType = function() {
-   return Blockly.Types.NUMBER;
+   return Blockly.Types.BOOLEAN;
 };
 Blockly.Blocks.ethernet_connected.getBlockType = function() {
-   return Blockly.Types.NUMBER;
+   return Blockly.Types.BOOLEAN;
 };
 Blockly.Blocks.ethernet_connect.getBlockType = function() {
    return Blockly.Types.NUMBER;
@@ -48,3 +48,23 @@ Blockly.Blocks.ethernet_get_request.getBlockType = function() {
 Blockly.Blocks.ethernet_post_request.getBlockType = function() {
    return Blockly.Types.NUMBER;
 };
+
+
+//****PARSER****
+Blockly.Blocks.ethernet_PARSER_end.getBlockType = function() {
+   return Blockly.Types.BOOLEAN;
+};
+Blockly.Blocks.ethernet_PARSER_count.getBlockType = function() {
+   return Blockly.Types.NUMBER;
+};
+Blockly.Blocks.ethernet_PARSER_getmyparam.getBlockType = function() {
+   return Blockly.Types.TEXT;
+};
+//****WIFI****
+Blockly.Blocks.ethernet_client_for_wifi_server.getBlockType = function() {
+   return Blockly.Types.BOOLEAN;
+};
+Blockly.Blocks.ethernet_wifi_localip.getBlockType = function() {
+   return Blockly.Types.NUMBER;
+};
+
