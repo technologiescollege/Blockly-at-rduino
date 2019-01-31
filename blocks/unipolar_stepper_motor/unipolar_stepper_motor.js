57e4fd4c8840f40d1118ca49a8bc1b234d7eb784
Ghislain Fabre <ghislain.fabre@open-elearning.com>
2019-02-01 00:10:45 +0100

    ajout d'un bloc pour moteur pas à pas unipolaire pour avancer le moteur en renseignant le nombre de tours et non de pas. Correction de la valeur par défaut pour le nombre de pas par révolution.

--- blocks/unipolar_stepper_motor/unipolar_stepper_motor.js
+++ blocks/unipolar_stepper_motor/unipolar_stepper_motor.js
@@ -65,7 +65,7 @@ Blockly.Blocks['unipolar_stepper_motor_init'] = { init: function() {
   }
 };
 
-//Bloc pour avancer
+//Bloc pour avancer en pas
 Blockly.Blocks['unipolar_stepper_motor_steps'] = { init: function() {
     this.appendValueInput("unipolar_stepper_motor_number_of_steps")
         .setCheck("Number")
@@ -78,7 +78,49 @@ Blockly.Blocks['unipolar_stepper_motor_steps'] = { init: function() {
             'unipolar_stepper_motor_variable')
         .appendField("de");
     this.appendDummyInput()
-        .appendField("pas.");
+        .appendField("PAS.");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
+    this.setColour(180);
+    this.setTooltip('Ce bloc sert à faire tourner le moteur pas à pas.');
+    this.setHelpUrl('https://www.arduino.cc/en/Reference/StepperStep');
+  },
+  /**
+   * Called whenever anything on the workspace changes.
+   * It checks/warns if the selected stepper instance has a config block.
+   * @this Blockly.Block
+   */
+  onchange: function() {
+    if (!this.workspace) return;  // Block has been deleted.
+
+    var instanceName = this.getFieldValue('STEPPER_NAME')
+    if (Blockly.Instances.isInstancePresent(instanceName, 'Stepper', this)) {
+      this.setWarningText(null);
+    } else {
+      // Set a warning to select a valid stepper config block
+      this.setWarningText(
+        Blockly.Msg.ARD_COMPONENT_WARN1.replace(
+            '%1', Blockly.Msg.STEPPER_COMPONENT).replace(
+                '%2', instanceName));
+    }
+  }
+};
+
+//Bloc pour avancer en tours
+Blockly.Blocks['unipolar_stepper_motor_turns'] = { init: function() {
+    this.appendValueInput("unipolar_stepper_motor_number_of_turns")
+        .setCheck("Number")
+        .appendField(new Blockly.FieldImage("blocks/unipolar_stepper_motor/unipolar_stepper_motor_init.png", 50, 50, "*"))
+        .appendField("AVANCER le moteur pas à pas unipolaire ")
+        .appendField(
+            new Blockly.FieldInstance('Stepper',
+                                      Blockly.Msg.STEPPER_DEFAULT_NAME,
+                                      false, false, false),
+            'unipolar_stepper_motor_variable')
+        .appendField("de");
+    this.appendDummyInput()
+        .appendField("TOUR(S).");
     this.setInputsInline(true);
     this.setPreviousStatement(true);
     this.setNextStatement(true);
