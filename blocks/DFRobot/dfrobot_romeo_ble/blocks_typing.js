fbf45c6966c9bf09ea06785fb794c65443c6b9f7
SebCanet <scanet@libreduc.cc>
2019-03-28 11:51:20 +0100

    Romeo_BLE error fixed

--- blocks/DFRobot/dfrobot_romeo_ble/blocks_typing.js
+++ blocks/DFRobot/dfrobot_romeo_ble/blocks_typing.js
@@ -1,24 +1,6 @@
 Blockly.Blocks.romeo_ble_switch.getBlockType = function() {
 	return Blockly.Types.BOOLEAN;
 };
-Blockly.Blocks.romeo_ble_forward.getBlockType = function() {
-	return Blockly.Types.NUMBER;
-};
-Blockly.Blocks.romeo_ble_backward.getBlockType = function() {
-	return Blockly.Types.NUMBER;
-};
-Blockly.Blocks.romeo_ble_turn_left.getBlockType = function() {
-	return Blockly.Types.NUMBER;
-};
-Blockly.Blocks.romeo_ble_turn_right.getBlockType = function() {
-	return Blockly.Types.NUMBER;
-};
-Blockly.Blocks.romeo_ble_mot_M1.getBlockType = function() {
-	return Blockly.Types.NUMBER;
-};
-Blockly.Blocks.romeo_ble_mot_M2.getBlockType = function() {
-	return Blockly.Types.NUMBER;
-};
 Blockly.Blocks.romeo_ble_M1_sens.getBlockType = function() {
 	return Blockly.Types.NUMBER;
 };
