75ba9f5fb284ab8a403330f1a997903b7300be21
SebCanet <scanet@libreduc.cc>
2024-10-04 17:17:39 +0200

    copy motors block in Stendhal category

--- blocks/stendhal/stendhal.js
+++ blocks/stendhal/stendhal.js
@@ -746,4 +746,76 @@ Blockly.Blocks.stendhal_dcmotor_v1 = {
   }
 };
 
+Blockly.Blocks['stendhal_l298n_motor_init'] = {
+  init: function() {
+    this.setColour(Blockly.Blocks.stendhal.HUE);
+	this.appendDummyInput()
+		.appendField(Blockly.Msg.ROBOTS_MOTORS_L298N_MOTOR_INIT_TITLE)
+		.appendField(new Blockly.FieldImage("blocks/robots/l298n.jpg", Blockly.Arduino.imageSize, Blockly.Arduino.imageSize));
+    this.appendDummyInput("")
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField(Blockly.Msg.ROBOTS_MOTORS_L298N_ID)
+        .appendField(new Blockly.FieldInstance('L298_ID', 'L298_ID', false, false, false), 'L298_NAME');
+	this.appendDummyInput()
+		.appendField(Blockly.Msg.ROBOTS_MOTORS_L298N_MOTOR_PIN_EN)
+        .appendField(new Blockly.FieldTextInput('0',  Blockly.Arduino.pinPWMValidator), 'PIN-EN')
+	    .setAlign(Blockly.ALIGN_RIGHT);
+	this.appendDummyInput()
+		.appendField(Blockly.Msg.ROBOTS_MOTORS_L298N_MOTOR_PIN_IN1)
+        .appendField(new Blockly.FieldTextInput('0',  Blockly.Arduino.pinDualValidator), 'PIN-IN1')
+		.appendField(Blockly.Msg.ROBOTS_MOTORS_L298N_MOTOR_PIN_IN2)
+        .appendField(new Blockly.FieldTextInput('0',  Blockly.Arduino.pinDualValidator), 'PIN-IN2')
+	    .setAlign(Blockly.ALIGN_RIGHT);
+	this.setInputsInline(false);
+	this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+	this.setTooltip(Blockly.Msg.ROBOTS_MOTORS_L298N_INIT_TOOLTIP);
+  }
+};
 
+Blockly.Blocks['stendhal_l298n_motor'] = {
+  init: function() {
+    this.setColour(Blockly.Blocks.stendhal.HUE);
+	this.appendDummyInput()
+		.appendField(Blockly.Msg.ROBOTS_MOTORS_L298N_MOTOR_TITLE)
+		.appendField(new Blockly.FieldImage("blocks/robots/l298n.jpg", Blockly.Arduino.imageSize, Blockly.Arduino.imageSize))
+		.appendField(
+				new Blockly.FieldDropdown([
+						[ Blockly.Msg.ROBOTS_MOTORS_L298N_STOP, "stop" ],
+						[ Blockly.Msg.ROBOTS_MOTORS_L298N_FORWARD, "forward" ],
+						[ Blockly.Msg.ROBOTS_MOTORS_L298N_BACKWARD, "backward" ],
+						[ Blockly.Msg.ROBOTS_MOTORS_L298N_BRAKE, "brake" ] ]),
+				"DIRECTION");
+    this.appendDummyInput("")
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField(Blockly.Msg.ROBOTS_MOTORS_L298N_ID)
+        .appendField(new Blockly.FieldInstance('L298_ID', 'L298_ID', false, false, false), 'L298_NAME');
+    this.appendValueInput("SPEED", 'Number')
+        .setCheck('Number')
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField(Blockly.Msg.ROBOTS_MOTORS_L298N_MOTOR_SPEED);
+	this.setInputsInline(false);
+	this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+	this.setTooltip(Blockly.Msg.ROBOTS_MOTORS_L298N_TOOLTIP);
+  },
+  /**
+   * Called whenever anything on the workspace changes.
+   * It checks/warns if the selected instance has a config block.
+   * @this Blockly.Block
+   */
+  onchange: function() {
+    if (!this.workspace) return;  // Block has been deleted.
+    var instanceName = this.getFieldValue('L298_NAME')
+    if (Blockly.Instances.isInstancePresent(instanceName, 'L298_ID', this)) {
+      this.setWarningText(null);
+    } else {
+      // Set a warning to select a valid config block
+      this.setWarningText(
+        Blockly.Msg.COMPONENT_WARN.replace(
+            //'%1', Blockly.Msg.SERVO_COMPONENT).replace(
+            '%1', '').replace(
+                '%2', instanceName));
+    }
+  }
+};
